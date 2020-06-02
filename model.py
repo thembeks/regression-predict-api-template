cec6529a084de838917e5d6d298437c1cc9c9070
thembeks <thembeka.zama@gmail.com>
2020-06-02 19:08:22 +0200

    model changesss

--- model.py
+++ model.py
@@ -49,7 +49,6 @@ def _preprocess_data(data):
     feature_vector_dict = json.loads(data)
     # Load the dictionary as a Pandas DataFrame.
     feature_vector_df = pd.DataFrame.from_dict([feature_vector_dict])
-    df_clean=feature_vector_df
     # ---------------------------------------------------------------
     # NOTE: You will need to swap the lines below for your own data
     # preprocessing methods.
@@ -59,89 +58,55 @@ def _preprocess_data(data):
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
-    df_rider = pd.read_csv(
-    'https://raw.githubusercontent.com/thembeks/Regression-Sendy-Logistics-Challenge-Team-14/Predict/Riders.csv')
+    predict_vector = feature_vector_df[['Order No', 'User Id', 'Vehicle Type', 'Platform Type',
+       'Personal or Business', 'Placement - Day of Month',
+       'Placement - Weekday (Mo = 1)', 'Placement - Time',
+       'Confirmation - Day of Month', 'Confirmation - Weekday (Mo = 1)',
+       'Confirmation - Time', 'Arrival at Pickup - Day of Month',
+       'Arrival at Pickup - Weekday (Mo = 1)', 'Arrival at Pickup - Time',
+       'Pickup - Day of Month', 'Pickup - Weekday (Mo = 1)', 'Pickup - Time',
+       'Distance (KM)', 'Temperature',
+       'Precipitation in millimeters', 'Pickup Lat', 'Pickup Long',
+       'Destination Lat', 'Destination Long', 'Rider Id']]
+    
+    
+    predict_vector.drop(['Vehicle Type','Order No'], axis=1, inplace=True)
 
-    df_train = pd.read_csv(
-    'https://raw.githubusercontent.com/thembeks/Regression-Sendy-Logistics-Challenge-Team-14/Predict/Train.csv')
 
-    #df_clean=df_train.copy()
-    df_clean = df_train.merge(df_rider, how='left', left_on = 'Rider Id', right_on = 'Rider Id')
+    predict_vector[['a','b', 'UserId No']] = predict_vector["User Id"].str.split("_", expand = True)
+    predict_vector[['c','d','RiderId No']] = predict_vector["Rider Id"].str.split("_",  expand = True)
 
-## convert time objects to datetime objects
-    df_clean['Placement - Time'] = pd.to_datetime(df_clean['Placement - Time'])
-    df_clean['Confirmation - Time'] = pd.to_datetime(df_clean['Confirmation - Time'])
-    df_clean['Arrival at Pickup - Time'] = pd.to_datetime(df_clean['Arrival at Pickup - Time'])
-    df_clean['Pickup - Time'] = pd.to_datetime(df_clean['Pickup - Time'])
+    predict_vector.drop(["User Id",'Rider Id', 'a', 'b','c','d'], axis=1, inplace = True)
+
+    predict_vector.rename(columns={'UserId No':'User Id'}, inplace=True)
+    predict_vector.rename(columns={'RiderId No':'Rider Id'}, inplace=True)
+
+    ## convert time objects to datetime objects
+    predict_vector['Placement - Time'] = pd.to_datetime(predict_vector['Placement - Time'])
+    predict_vector['Confirmation - Time'] = pd.to_datetime(predict_vector['Confirmation - Time'])
+    predict_vector['Arrival at Pickup - Time'] = pd.to_datetime(predict_vector['Arrival at Pickup - Time'])
+    predict_vector['Pickup - Time'] = pd.to_datetime(predict_vector['Pickup - Time'])
 
 ### change time variables to difference in time in seconds to keep time stationary and appropriate for our model
-    df_clean['Time Placement to Confirmation'] = (df_clean['Confirmation - Time'] - df_clean['Placement - Time']).dt.seconds
-    df_clean['Time Confirmation to PickupArrival'] = (df_clean['Arrival at Pickup - Time'] - df_clean['Confirmation - Time']).dt.seconds
-    df_clean['Time Arrival to Pickup'] = (df_clean['Pickup - Time'] - df_clean['Arrival at Pickup - Time']).dt.seconds
+    predict_vector['Time Placement to Confirmation'] = (predict_vector['Confirmation - Time'] - predict_vector['Placement - Time']).dt.seconds
+    predict_vector['Time Confirmation to PickupArrival'] = (predict_vector['Arrival at Pickup - Time'] - predict_vector['Confirmation - Time']).dt.seconds
+    predict_vector['Time Arrival to Pickup'] = (predict_vector['Pickup - Time'] - predict_vector['Arrival at Pickup - Time']).dt.seconds
+
+    predict_vector.drop(['Pickup - Time','Arrival at Pickup - Time','Confirmation - Time','Placement - Time','Rider Id','User Id'], axis=1, inplace=True)
+
+    #cols = list(copy.columns.values)
+    #cols.pop(cols.index('Time from Pickup to Arrival')) 
+
+    #copy = copy[cols+['Time from Pickup to Arrival']]
+
+    #Ecode ctagorical column
+    predict_vector['Personal or Business'].unique()
+    Bdict = {'Personal': 0, 'Business': 1}
+    predict_vector['Personal or Business'] = predict_vector['Personal or Business'].map(Bdict)
 
 
-### drop the old columns. Note we will keep one for analysis purposes 'Placement_Time'
-    df_clean.drop(['Confirmation - Time', 'Arrival at Pickup - Time', "Pickup - Time"], axis= 1, inplace=True)
-
-## drop values that are different as they are the outliers
-    df_clean.drop(df_clean[df_clean['Placement - Day of Month'] != df_clean['Confirmation - Day of Month']].index, inplace = True)
-
-## drop all other columns relating to the day of month and day of week
-    df_clean.drop(['Placement - Day of Month','Placement - Weekday (Mo = 1)', 'Confirmation - Day of Month', 'Confirmation - Weekday (Mo = 1)', 'Arrival at Pickup - Day of Month',
-       'Arrival at Pickup - Weekday (Mo = 1)', 'Pickup - Day of Month', 'Pickup - Weekday (Mo = 1)'], axis = 1, inplace = True)
-
-## We will drop the columns that are not in the test data set to keep consistency
-    df_clean.drop(['Arrival at Destination - Day of Month', 'Arrival at Destination - Weekday (Mo = 1)', 'Arrival at Destination - Time'], axis = 1, inplace = True)
-
-## Note that for Placement Time column we just want the time and not date
-    df_clean['Placement - Time'] = df_clean['Placement - Time'].dt.time
-
-## Note we need to convert platform type from numeric to categorical since its categorical
-    df_clean['Platform Type'] = df_clean['Platform Type'].map({3: 'plat 3', 1: 'plat 1', 2: 'plat 2', 4:'plat 4'})
-
-
-#df_clean['Temperature'] = round(df_clean.groupby(['Day of Month'])['Temperature'].apply(lambda x: x.fillna(x.median())), 1)
-    df_clean['Precipitation in millimeters'].fillna(0.0, inplace = True)
-    df_clean['Temperature'].fillna(0.0, inplace = True)
-
-    df_clean.drop(['Order No', 'User Id', 'Rider Id', 'Placement - Time'], axis =1, inplace=True)
-    df_clean = pd.get_dummies(df_clean, drop_first = True)
-
-
-
-#df_sig_test = df_clean.copy()
-
-#df_sig_test.drop(['Platform_Type_plat_2', 'Platform_Type_plat_3', 'Platform_Type_plat_4','Personal_or_Business_Personal'], axis = 1, inplace = True)
-
-    df_train_clean = df_clean.copy()
-    q = df_train_clean['Time from Pickup to Arrival'].quantile(0.98)
-    data_1 = df_train_clean[(df_train_clean['Time from Pickup to Arrival']<q) & (df_train_clean['Time from Pickup to Arrival'] > 0)]
-#sns.distplot(data_1['Time_from_Pickup_to_Arrival'])
-
-    p = df_train_clean['No_Of_Orders'].quantile(0.99)
-    data_2 = data_1[data_1['No_Of_Orders']<p]
-#sns.distplot(data_2['No_Of_Orders'])
-
-    u = df_train_clean['Time Placement to Confirmation'].quantile(0.99)
-    data_3 = data_2[(data_2['Time Placement to Confirmation']<u) & (data_2['Time Placement to Confirmation']>0)]
-    df_no_outliers = data_3
-
-
-    cols = list(df_no_outliers.columns.values)
-    cols.pop(cols.index('Time from Pickup to Arrival')) 
-
-    df_no_outliers  = df_no_outliers [cols+['Time from Pickup to Arrival']]
-
-    model_features=['Distance (KM)', 'Temperature', 'Precipitation in millimeters',
-       'Pickup Lat', 'Pickup Long', 'Destination Lat', 'Destination Long',
-       'No_Of_Orders', 'Age', 'Average_Rating', 'No_of_Ratings',
-       'Time Placement to Confirmation', 'Time Confirmation to PickupArrival',
-       'Time Arrival to Pickup', 'Platform Type_plat 2',
-       'Platform Type_plat 3', 'Platform Type_plat 4',
-       'Personal or Business_Personal', 'Time from Pickup to Arrival']
-
-    df_no_outliers=df_no_outliers[model_features]
-    return df_no_outliers
+    predict_vector.fillna(predict_vector.mean(), inplace=True)
+    return predict_vector
 
 def load_model(path_to_model:str):
     """Adapter function to load our pretrained model into memory.
--- utils/train_model.py
+++ utils/train_model.py
@@ -15,101 +15,77 @@ import pickle
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
+# Fetch training data and preprocess for modeling
+train= pd.read_csv('https://raw.githubusercontent.com/thembeks/Regression-Sendy-Logistics-Challenge-Team-14/Predict/Train.csv')
 
-df_rider = pd.read_csv(
-    'https://raw.githubusercontent.com/thembeks/Regression-Sendy-Logistics-Challenge-Team-14/Predict/Riders.csv')
 
-df_train = pd.read_csv(
-    'https://raw.githubusercontent.com/thembeks/Regression-Sendy-Logistics-Challenge-Team-14/Predict/Train.csv')
+#Drop columns not in test
+copy=train.copy()
+copy.drop(['Vehicle Type','Arrival at Destination - Day of Month','Arrival at Destination - Weekday (Mo = 1)','Arrival at Destination - Time', 'Order No'], axis=1, inplace=True)
 
-df_clean=df_train.copy()
-df_clean = df_train.merge(df_rider, how='left', left_on = 'Rider Id', right_on = 'Rider Id')
+#Remove strings in User ID and Ride Id
+copy[['a','b', 'UserId No']] = copy["User Id"].str.split("_", expand = True)
+copy[['c','d','RiderId No']] = copy["Rider Id"].str.split("_",  expand = True)
+
+copy.drop(["User Id",'Rider Id', 'a', 'b','c','d'], axis=1, inplace = True)
+
+copy.rename(columns={'UserId No':'User Id'}, inplace=True)
+copy.rename(columns={'RiderId No':'Rider Id'}, inplace=True)
 
 ## convert time objects to datetime objects
-df_clean['Placement - Time'] = pd.to_datetime(df_clean['Placement - Time'])
-df_clean['Confirmation - Time'] = pd.to_datetime(df_clean['Confirmation - Time'])
-df_clean['Arrival at Pickup - Time'] = pd.to_datetime(df_clean['Arrival at Pickup - Time'])
-df_clean['Pickup - Time'] = pd.to_datetime(df_clean['Pickup - Time'])
+copy['Placement - Time'] = pd.to_datetime(copy['Placement - Time'])
+copy['Confirmation - Time'] = pd.to_datetime(copy['Confirmation - Time'])
+copy['Arrival at Pickup - Time'] = pd.to_datetime(copy['Arrival at Pickup - Time'])
+copy['Pickup - Time'] = pd.to_datetime(copy['Pickup - Time'])
 
 ### change time variables to difference in time in seconds to keep time stationary and appropriate for our model
-df_clean['Time Placement to Confirmation'] = (df_clean['Confirmation - Time'] - df_clean['Placement - Time']).dt.seconds
-df_clean['Time Confirmation to PickupArrival'] = (df_clean['Arrival at Pickup - Time'] - df_clean['Confirmation - Time']).dt.seconds
-df_clean['Time Arrival to Pickup'] = (df_clean['Pickup - Time'] - df_clean['Arrival at Pickup - Time']).dt.seconds
+copy['Time Placement to Confirmation'] = (copy['Confirmation - Time'] - copy['Placement - Time']).dt.seconds
+copy['Time Confirmation to PickupArrival'] = (copy['Arrival at Pickup - Time'] - copy['Confirmation - Time']).dt.seconds
+copy['Time Arrival to Pickup'] = (copy['Pickup - Time'] - copy['Arrival at Pickup - Time']).dt.seconds
 
 
 ### drop the old columns. Note we will keep one for analysis purposes 'Placement_Time'
-df_clean.drop(['Confirmation - Time', 'Arrival at Pickup - Time', "Pickup - Time"], axis= 1, inplace=True)
-
-## drop values that are different as they are the outliers
-df_clean.drop(df_clean[df_clean['Placement - Day of Month'] != df_clean['Confirmation - Day of Month']].index, inplace = True)
-
-## drop all other columns relating to the day of month and day of week
-df_clean.drop(['Placement - Day of Month','Placement - Weekday (Mo = 1)', 'Confirmation - Day of Month', 'Confirmation - Weekday (Mo = 1)', 'Arrival at Pickup - Day of Month',
-       'Arrival at Pickup - Weekday (Mo = 1)', 'Pickup - Day of Month', 'Pickup - Weekday (Mo = 1)'], axis = 1, inplace = True)
-
-## We will drop the columns that are not in the test data set to keep consistency
-df_clean.drop(['Arrival at Destination - Day of Month', 'Arrival at Destination - Weekday (Mo = 1)', 'Arrival at Destination - Time'], axis = 1, inplace = True)
-
-## Note that for Placement Time column we just want the time and not date
-df_clean['Placement - Time'] = df_clean['Placement - Time'].dt.time
-
-## Note we need to convert platform type from numeric to categorical since its categorical
-df_clean['Platform Type'] = df_clean['Platform Type'].map({3: 'plat 3', 1: 'plat 1', 2: 'plat 2', 4:'plat 4'})
-
-
-#df_clean['Temperature'] = round(df_clean.groupby(['Day of Month'])['Temperature'].apply(lambda x: x.fillna(x.median())), 1)
-df_clean['Precipitation in millimeters'].fillna(0.0, inplace = True)
-df_clean['Temperature'].fillna(0.0, inplace = True)
-
-df_clean.drop(['Order No', 'User Id', 'Rider Id', 'Placement - Time'], axis =1, inplace=True)
-df_clean = pd.get_dummies(df_clean, drop_first = True)
+#.drop(['Confirmation - Time', 'Arrival at Pickup - Time', "Pickup - Time"], axis= 1, inplace=True)
 
 
 
-#df_sig_test = df_clean.copy()
+#Drop Columns
+copy.drop(['Pickup - Time','Arrival at Pickup - Time','Confirmation - Time','Placement - Time', 'Rider Id','User Id'], axis=1, inplace=True)
 
-#df_sig_test.drop(['Platform_Type_plat_2', 'Platform_Type_plat_3', 'Platform_Type_plat_4','Personal_or_Business_Personal'], axis = 1, inplace = True)
-
-df_train_clean = df_clean.copy()
-q = df_train_clean['Time from Pickup to Arrival'].quantile(0.98)
-data_1 = df_train_clean[(df_train_clean['Time from Pickup to Arrival']<q) & (df_train_clean['Time from Pickup to Arrival'] > 0)]
-#sns.distplot(data_1['Time_from_Pickup_to_Arrival'])
-
-p = df_train_clean['No_Of_Orders'].quantile(0.99)
-data_2 = data_1[data_1['No_Of_Orders']<p]
-#sns.distplot(data_2['No_Of_Orders'])
-
-u = df_train_clean['Time Placement to Confirmation'].quantile(0.99)
-data_3 = data_2[(data_2['Time Placement to Confirmation']<u) & (data_2['Time Placement to Confirmation']>0)]
-df_no_outliers = data_3
-
-
-cols = list(df_no_outliers.columns.values)
+#Rearrange dataframe
+cols = list(copy.columns.values)
 cols.pop(cols.index('Time from Pickup to Arrival')) 
 
-df_no_outliers  = df_no_outliers [cols+['Time from Pickup to Arrival']]
+copy = copy[cols+['Time from Pickup to Arrival']]
 
-model_features=['Distance (KM)', 'Temperature', 'Precipitation in millimeters',
-       'Pickup Lat', 'Pickup Long', 'Destination Lat', 'Destination Long',
-       'No_Of_Orders', 'Age', 'Average_Rating', 'No_of_Ratings',
-       'Time Placement to Confirmation', 'Time Confirmation to PickupArrival',
-       'Time Arrival to Pickup', 'Platform Type_plat 2',
-       'Platform Type_plat 3', 'Platform Type_plat 4',
-       'Personal or Business_Personal', 'Time from Pickup to Arrival']
+#Ecode categorical column
+copy['Personal or Business'].unique()
+Bdict = {'Personal': 0, 'Business': 1}
+copy['Personal or Business'] = copy['Personal or Business'].map(Bdict)
 
+#Replace NAN values
+copy= copy.replace(np.nan, 0)
 
-x= df_no_outliers.iloc[:, :-1].values
-y = df_no_outliers.iloc[:, -1].values
+x = copy.drop(['Time from Pickup to Arrival'], axis=1)
+y = copy['Time from Pickup to Arrival']
+
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 50)
 
 
-reg = LinearRegression()
-reg.fit(x_train, y_train)
+
+# Fit model
+
+
+from sklearn.linear_model import LinearRegression
+lm = LinearRegression(normalize=True)
+lm.fit(X_train, y_train)
+
+# Pickle model for use within our API
 
 import pickle
 
-model_save_path = "new_model.pkl"
+model_save_path = "newer_model.pkl"
 with open(model_save_path,'wb') as file:
-    pickle.dump(reg,file)
+    pickle.dump(lm,file)
